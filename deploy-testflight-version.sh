a88a96f8701c2c4807d6fb0e04e3adc8d01f8168
Mark Burggraf <markb@mantisbible.com>
2024-10-25 06:49:10 -0700

    added deploy-testflight-version.sh

--- deploy-testflight-version.sh
+++ deploy-testflight-version.sh
@@ -1,6 +1,7 @@
 npm run build && npx cap sync && npx cap update ios && npx cap copy ios
 cd ./ios/App
 set -a
+# pick up env var from .env file: FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD
 source .env
 set +a
 fastlane beta
